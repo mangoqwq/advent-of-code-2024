488dfc9a8b06e50e1be1ddc5df89db11ae56d257
Max Jiang <maxjiang123@gmail.com>
2024-12-02 00:23:24 -0500

    Day 2 🏭

--- 02b-red-nosed-reports.cpp
+++ 02b-red-nosed-reports.cpp
@@ -13,36 +13,36 @@ int main() {
 	cin.tie(0)->sync_with_stdio(0);
 	string line;
 	ll ans = 0;
-    auto safe = [&](vector<int> v) -> bool {
-        vector<int> f = v;
-        vector<int> r = v;
-        sort(f.begin(), f.end());
-        sort(r.begin(), r.end(), greater<int>());
-        if (v != f && v != r) return 0;
-        bool ok = 1;
-        for (int i = 1; i < (int)v.size(); ++i) {
-            ok &= abs(v[i] - v[i-1]) <= 3 && abs(v[i] - v[i-1]) > 0;
-        }
-        return ok;
-    };
+	auto safe = [&](vector<int> v) -> bool {
+		vector<int> f = v;
+		vector<int> r = v;
+		sort(f.begin(), f.end());
+		sort(r.begin(), r.end(), greater<int>());
+		if (v != f && v != r) return 0;
+		bool ok = 1;
+		for (int i = 1; i < (int)v.size(); ++i) {
+			ok &= abs(v[i] - v[i-1]) <= 3 && abs(v[i] - v[i-1]) > 0;
+		}
+		return ok;
+	};
 
 	while (getline(cin, line)) {
 		istringstream iss(line);
-        vector<int> v;
-        int x;
-        while (iss >> x) v.push_back(x);
-        if (safe(v)) {
-            ans++;
-            continue;
-        }
-        for (int i = 0; i < (int)v.size(); ++i) {
-            auto cur = v;
-            cur.erase(cur.begin() + i);
-            if (safe(cur)) {
-                ans++;
-                break;
-            }
-        }
+		vector<int> v;
+		int x;
+		while (iss >> x) v.push_back(x);
+		if (safe(v)) {
+			ans++;
+			continue;
+		}
+		for (int i = 0; i < (int)v.size(); ++i) {
+			auto cur = v;
+			cur.erase(cur.begin() + i);
+			if (safe(cur)) {
+				ans++;
+				break;
+			}
+		}
 	}
 	cout << ans << '\n';
 }
