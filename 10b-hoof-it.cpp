4d1060b5a8a441ff49992aee559f9ca4a52f8ed9
Max Jiang <maxjiang123@gmail.com>
2024-12-10 00:22:19 -0500

    Day 10 ⛰️

---
+++ 10b-hoof-it.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+#ifdef _DEBUG
+#include "utils/debug.h"
+#else
+#define debug(...) 0
+#endif
+
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	string line;
+	ll ans = 0;
+	vector<string> g;
+	while (getline(cin, line)) {
+		g.push_back(line);
+	}
+	int N = g.size(), M = g[0].size();
+	auto in = [&](int i, int j) { return i >= 0 && j >= 0 && i < N && j < M; };
+	auto go = [&](auto self, int i, int j, int d) -> void {
+		if (d == 9) ans++;
+		else {
+			for (int dir = 0; dir < 4; ++dir) {
+				int nx = i + dx[dir], ny = j + dy[dir];
+				if (!in(nx, ny)) continue;
+				if (g[nx][ny] - '0' != d + 1) continue;
+				self(self, nx, ny, d + 1);
+			}
+		}
+	};
+	for (int i = 0; i < N; ++i) {
+		for (int j = 0; j < M; ++j) {
+			if (g[i][j] == '0') go(go, i, j, 0);
+		}
+	}
+	cout << ans << '\n';
+}
